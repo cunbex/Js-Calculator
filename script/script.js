771bc6310025d293a41ca6e0a6308db1a7133b43
cunbex <hamzalagab.tech@gmail.com>
2023-03-19 11:19:03 +0100

    fix html css and js.
    added calculate function to make calculator work, fixed floatPoint
    function for decimals, added writeData and writeResult function,
    added backspace function to remove numbers.
    also refactored other functions.

--- script/script.js
+++ script/script.js
@@ -3,10 +3,12 @@ const display = document.getElementById("display");
 const n1 = document.getElementById("n1");
 const op = document.getElementById("op");
 const n2 = document.getElementById("n2");
+const rs = document.getElementById("result");
 const operate = {
   firstNumber: "",
   operation: "",
   secondNumber: "",
+  result: "",
 };
 buttons.forEach((button) => {
   switch (button.classList.value) {
@@ -41,49 +43,94 @@ function clear() {
   n1.innerText = "";
   op.innerText = "";
   n2.innerText = "";
-  console.log(operate);
 }
 function floatPoint() {
   switch (true) {
-    case operate.firstNumber === "" || operate.secondNumber === "":
+    case operate.firstNumber === "" ||
+      (operate.secondNumber === "" && operate.operation !== ""):
       confirm("Enter a number first");
       break;
     case operate.firstNumber !== "" &&
       !operate.firstNumber.includes(".") &&
       operate.operation === "":
       operate.firstNumber += ".";
-      n1.innerText += ".";
       break;
     case operate.firstNumber !== "" &&
       operate.operation !== "" &&
       !operate.secondNumber.includes("."):
       operate.secondNumber += ".";
-      n2.innerText += ".";
       break;
     default:
       confirm("Float point already exist");
       break;
   }
+  writeData();
 }
-function backspace(e) {
-  console.log(e.target.textContent);
+function backspace() {
+  if (operate.secondNumber !== "") {
+    operate.secondNumber = operate.secondNumber.substring(
+      0,
+      operate.secondNumber.length - 1
+    );
+  } else if (operate.secondNumber === "" && operate.operation !== "") {
+    operate.operation = "";
+  } else if (operate.firstNumber !== "" && operate.operation === "") {
+    operate.firstNumber = operate.firstNumber.substring(
+      0,
+      operate.firstNumber.length - 1
+    );
+  }
+  writeData();
 }
-function calculate(e) {
-  console.log(operate);
-}
-function digit(e) {
-  switch (true) {
-    case operate.operation === "":
-      operate.firstNumber += e.target.textContent;
-      n1.innerText = operate.firstNumber;
+function calculate() {
+  nb1 = parseFloat(operate.firstNumber);
+  nb2 = parseFloat(operate.secondNumber);
+  switch (operate.operation) {
+    case "*":
+      operate.result = nb1 * nb2;
       break;
-    case operate.operation !== "" && operate.operation !== NaN:
-      operate.secondNumber += e.target.textContent;
-      n2.innerText = operate.secondNumber;
+    case "/":
+      if (nb2 === 0) {
+        confirm("can't divide by 0");
+      }
+      operate.result = nb1 / nb2;
+      break;
+    case "+":
+      operate.result = nb1 + nb2;
+      break;
+    case "-":
+      operate.result = nb1 - nb2;
+      break;
     default:
       break;
   }
-  console.log(operate);
+  writeResult();
+}
+function digit(e) {
+  switch (true) {
+    case (operate.secondNumber.length > 2 &&
+      !operate.secondNumber.includes(".")) ||
+      (operate.firstNumber.length > 2 && !operate.firstNumber.includes(".")):
+      confirm(
+        "Please keep the numbers in 10's max for the clean layout, if this keeps prompting use backspace to remove 1 digit"
+      );
+      return;
+    case (operate.secondNumber.length > 5 &&
+      operate.secondNumber.includes(".")) ||
+      (operate.firstNumber.length > 5 && operate.firstNumber.includes(".")):
+      confirm(
+        "Please keep the numbers in 10's max for the clean layout, if this keeps prompting use backspace to remove 1 digit"
+      );
+      return;
+    case operate.operation === "":
+      operate.firstNumber += e.target.textContent;
+      break;
+    case operate.operation !== "" && operate.operation !== NaN:
+      operate.secondNumber += e.target.textContent;
+    default:
+      break;
+  }
+  writeData();
 }
 function operation(e) {
   switch (true) {
@@ -95,10 +142,23 @@ function operation(e) {
     case e.target.className === "operator" &&
       (operate.firstNumber !== "" || operate.secondNumber !== ""):
       operate.operation = e.target.textContent;
-      op.innerText = operate.operation;
       break;
     default:
       alert("error in operation");
       break;
+  }
+  writeData();
+}
+
+function writeData() {
+  n1.innerText = operate.firstNumber;
+  n2.innerText = operate.secondNumber;
+  op.innerText = operate.operation;
+}
+function writeResult() {
+  if (operate.firstNumber.includes(".") || operate.secondNumber.includes(".")) {
+    rs.innerText = operate.result.toFixed(3);
+  } else {
+    rs.innerText = operate.result;
   }
 }
